102827da66fbed2f7afc58d82d525fd0aeb6c746
Kian <shanekiantiu@gmail.com>
2022-07-07 11:48:09 +0800

    Add isLoading and textControllers to Note Modify view

--- lib/views/note_modify.dart
+++ lib/views/note_modify.dart
@@ -20,15 +20,29 @@ class _NoteModifyState extends State<NoteModify> {
   String? errorMessage;
   late Note note;
 
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+
+  bool _isLoading = false;
+
   @override
   void initState() {
+    super.initState();
+    setState(() {
+      _isLoading = true;
+    });
+
     notesService.getNote(widget.noteID!).then((response) {
+      setState(() {
+        _isLoading = false;
+      });
       if (response!.error) {
         errorMessage = response.errorMessage ?? 'An error occurred.';
       }
       note = response.data!;
+      _titleController.text = note.noteTitle!;
+      _contentController.text = note.noteContent!;
     });
-    super.initState();
   }
 
   @override
@@ -37,33 +51,37 @@ class _NoteModifyState extends State<NoteModify> {
       appBar: AppBar(title: Text(isEditing ? 'Edit note' : 'Create note')),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: <Widget>[
-            const TextField(
-              decoration: InputDecoration(hintText: 'Note title'),
-            ),
-            Container(height: 8),
-            const TextField(
-              decoration: InputDecoration(hintText: 'Note content'),
-            ),
-            Container(height: 16),
-            SizedBox(
-              width: double.infinity,
-              height: 35,
-              child: ElevatedButton(
-                child: const Text('Submit'),
-                onPressed: () {
-                  if (isEditing) {
-                    // update note in api
-                  } else {
-                    // create not in api
-                  }
-                  Navigator.of(context).pop();
-                },
+        child: _isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : Column(
+                children: <Widget>[
+                  TextField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(hintText: 'Note title'),
+                  ),
+                  Container(height: 8),
+                  TextField(
+                    controller: _contentController,
+                    decoration: const InputDecoration(hintText: 'Note content'),
+                  ),
+                  Container(height: 16),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 35,
+                    child: ElevatedButton(
+                      child: const Text('Submit'),
+                      onPressed: () {
+                        if (isEditing) {
+                          // update note in api
+                        } else {
+                          // create not in api
+                        }
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
       ),
     );
   }
