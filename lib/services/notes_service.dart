77bbd3d4726702f72ab4df15b8071ec1a9e47c1b
Kian <shanekiantiu@gmail.com>
2022-07-07 11:34:35 +0800

    Replace getNotesList with http request

--- lib/services/notes_service.dart
+++ lib/services/notes_service.dart
@@ -1,8 +1,33 @@
+import 'dart:convert';
+
+import '../models/api_response.dart';
 import '../models/note_for_listing.dart';
+import 'package:http/http.dart' as http;
 
 class NoteService {
-  Future<List<NoteForListing>> getNotesList() {
+  static const api = 'https://tq-notes-api-jkrgrdggbq-el.a.run.app';
+  static const headers = {
+    'apiKey': 'e7ec8231-b9fc-4d2e-9243-c5dc3adc1b44',
+  };
 
+  Future<APIResponse<List<NoteForListing>>?> getNotesList() {
+    return http.get(Uri.parse('$api/notes'), headers: headers).then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        final notes = <NoteForListing>[];
+        for (var item in jsonData) {
+          final note = NoteForListing(
+            noteID: item['noteID'],
+            noteTitle: item['noteTitle'],
+            createDateTime: DateTime.parse(item['createDateTime']),
+            latestEditDateTime: item['latestEditDateTime'] != null
+                ? DateTime.parse(item['latestEditDateTime'])
+                : null,
+          );
+          notes.add(note);
+        }
+        return APIResponse<List<NoteForListing>>(data: notes);
+      }
+    }).catchError((_) => APIResponse<List<NoteForListing>>(error: true, errorMessage: 'An error occurred.'));
   }
-
 }
